02f59efaa0666878cd897e0031843e8ed9f62cbf
Anenokil <anenokil.w@gmail.com>
2025-04-05 15:31:44 +0300

    Codestyle: add docstring

--- src/EDA_and_preprocessing.py
+++ src/EDA_and_preprocessing.py
@@ -2,6 +2,12 @@ import pandas as pd
 
 
 def process_na(df: pd.DataFrame) -> dict[str, ...]:
+    """
+    Process missed values in data
+
+    :param df: data
+    :return: dict with keys: 'result' - processed data, 'stat': percentage of missed values
+    """
     stat = df.isna().sum(axis=0) / df.shape[0]
     new_df = df.dropna()
     return {'result': new_df, 'stat': stat}
