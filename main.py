08bf9ae849f48d14d3801a141bc5827458d1692b
Anenokil <anenokil.w@gmail.com>
2025-04-05 16:13:22 +0300

    Implement categorical features processing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 
 from src.data_provider import DataProvider
 from src.data_collector import DataCollector, data_to_xy
-from src.EDA import process_na
+from src.EDA_and_preprocessing import process
 
 TARGET = 'WITH_PAID'
 
@@ -23,7 +23,7 @@ def main():
     data_collector = DataCollector()
     while True:
         data = data_provider.get_batch()
-        data = process_na(data)['result']
+        data, stat = process(data)
         x, y = data_to_xy(data, TARGET)
         data_collector.add(x, y)
         print(data_collector.x)
--- src/EDA_and_preprocessing.py
+++ src/EDA_and_preprocessing.py
@@ -1,13 +1,50 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 
 def process_na(df: pd.DataFrame) -> dict[str, ...]:
     """
-    Process missed values in data
+    Process missed values - removes them
 
     :param df: data
     :return: dict with keys: 'result' - processed data, 'stat': percentage of missed values
     """
     stat = df.isna().sum(axis=0) / df.shape[0]
-    new_df = df.dropna()
-    return {'result': new_df, 'stat': stat}
+    res = df.dropna()
+    return {'result': res, 'stat': stat}
+
+
+def process_ctg(df: pd.DataFrame) -> dict[str, ...]:
+    """
+    Process categorical features - encode them
+
+    :param df: data
+    :return: dict with keys: 'result' - processed data, 'stat': names of categorical features
+    """
+    ohe = OneHotEncoder()
+    categorical = df.dtypes[df.dtypes == 'object'].index.tolist()
+    noncategorical = df.drop(categorical, axis=1)
+    encoded = ohe.fit_transform(df[categorical])
+    encoded = pd.DataFrame(encoded.toarray(), columns=ohe.get_feature_names_out(),
+                           dtype=int, index=noncategorical.index)
+    res = pd.concat([noncategorical, encoded], axis=1)
+    return {'result': res, 'stat': categorical}
+
+
+def process(df: pd.DataFrame) -> tuple[pd.DataFrame, dict[str, ...]]:
+    """
+    Process data
+
+    :param df: data
+    :return: processed data and dict with information about data
+    """
+    stat = {}
+    data = df.copy()
+
+    res = process_na(data)
+    data, stat['na'] = res['result'], res['stat']
+
+    res = process_ctg(data)
+    data, stat['ctg'] = res['result'], res['stat']
+
+    return data, stat
