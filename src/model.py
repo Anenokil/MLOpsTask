876c12bc2e8f2f75ecd5efe7b3c49eba6c6a04f3
Anenokil <anenokil.w@gmail.com>
2025-04-07 05:19:31 +0300

    Codestyle: typing and docstrings

--- src/model.py
+++ src/model.py
@@ -1,9 +1,16 @@
+import typing
 import pandas as pd
+
 from src.data_collector import DataCollector
 
 
 class Model:
     def __init__(self, model):
+        """
+        Model trains, evaluates, and validates model
+
+        :param model: ML model from sklearn
+        """
         self.model = model
         self.data = DataCollector()
 
@@ -11,11 +18,11 @@ class Model:
         self.data.add(x, y)
         self.model.fit(*self.data.get())
 
-    def predict(self, x: pd.DataFrame):
+    def predict(self, x: pd.DataFrame) -> typing.Any:
         return self.model.predict(x)
 
-    def eval(self, x: pd.DataFrame, y: pd.DataFrame):
+    def eval(self, x: pd.DataFrame, y: pd.DataFrame) -> float:
         return self.model.score(x, y)
 
-    def is_fit(self):
+    def is_fit(self) -> bool:
         return self.data.x is not None
