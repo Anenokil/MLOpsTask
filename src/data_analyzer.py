5735e00f96df7d17cfa852f2916e0f6ba88e6dd4
Anenokil <anenokil.w@gmail.com>
2025-04-10 16:55:28 +0300

    Analyze columns' statistics

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -19,6 +19,18 @@ class DataAnalyzer:
         rows_with_na = na_by_row[na_by_row != 0].shape[0] / self.data.shape[0]
         self.stat['na'] = {'na_by_col': na_by_col, 'rows_with_na': rows_with_na}
 
+    def __analyze_col_stats(self):
+        """
+        Collect statistics on completed values
+        """
+        categorical_cols = self.data.dtypes[self.data.dtypes == 'object'].index.tolist()
+        categorical = self.data[categorical_cols]
+        noncategorical = self.data.drop(categorical_cols, axis=1)
+        self.stat['ctg_modes'] = categorical.mode(axis=0).to_numpy()[0]
+        self.stat['num_medians'] = noncategorical.median()
+        self.stat['num_means'] = noncategorical.mean()
+        self.stat['num_vars'] = noncategorical.var()
+
     def analyze(self, df: pd.DataFrame) -> dict[str, typing.Any]:
         """
         Analyze data
@@ -31,5 +43,6 @@ class DataAnalyzer:
 
         # Analyze missing values
         self.__analyze_na()
+        self.__analyze_col_stats()
 
         return self.stat
